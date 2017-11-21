b02b4d22f1e1420e877a4b6040426b4cde07eb01
Antwan van Houdt <jabwd@exurion.com>
2017-11-21 12:03:31 +0100

    More headers

--- shim.h
+++ shim.h
@@ -1,8 +1,13 @@
 #ifndef LIBRESSL_SHIM_H
 #define LIBRESSL_SHIM_H
 
-#include <openssl/ssl.h>
+#include <openssl/bio.h>
 #include <openssl/err.h>
+#include <openssl/evp.h>
+#include <openssl/pem.h>
+#include <openssl/safestack.h>
+#include <openssl/ssl.h>
+#include <openssl/x509.h>
 #include <tls.h>
 
 #endif
